1fd69b7ef80483b1acc1344cedf5a2f8fb9e453a
mazentamer <mazentamer74@yahoo.com>
2021-09-30 12:43:34 +0200

    Create USART_Address.h

---
+++ FinalProject_Master/FinalProject_Master/MCAL/USART_Address.h
@@ -0,0 +1,21 @@
+/*
+ * USART_Address.h
+ *
+ * Created: 03/09/2021 21:24:22
+ *  Author: user
+ */ 
+
+
+#ifndef USART_ADDRESS_H_
+#define USART_ADDRESS_H_
+
+#include "CPU_Configurations.h"
+/*
+#define UDR		(*(volatile uint8_t*)0x2C)
+#define UCSRA	(*(volatile uint8_t*)0x2B)
+#define UCSRB	(*(volatile uint8_t*)0x2A)
+#define UCSRC	(*(volatile uint8_t*)0x40)
+#define UBBRL	(*(volatile uint8_t*)0x29)
+#define UBBRH	UCSRC
+*/
+#endif /* USART_ADDRESS_H_ */
