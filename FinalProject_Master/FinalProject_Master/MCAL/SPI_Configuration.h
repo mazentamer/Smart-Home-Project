f3cc73afa4db121c022d432528ba3088d9b8ac92
mazentamer <mazentamer74@yahoo.com>
2021-09-30 12:43:28 +0200

    Create SPI_Configuration.h

---
+++ FinalProject_Master/FinalProject_Master/MCAL/SPI_Configuration.h
@@ -0,0 +1,27 @@
+/*
+ * SPI_Configuration.h
+ *
+ * Created: 04/09/2021 21:45:58
+ *  Author: user
+ */ 
+
+
+#ifndef SPI_CONFIGURATION_H_
+#define SPI_CONFIGURATION_H_
+
+#include "SPI_Address.h"
+
+#define SPI_INT_OFF		0
+#define SPI_INT_ON		1
+
+#define SPI_DORD_LSB	0
+#define SPI_DORD_MSB	1		
+
+#define SPI_SLAVE		0
+#define SPI_MASTER		1
+
+#define SPI_INT_MODE	SPI_INT_OFF
+#define SPI_M_OR_S		SPI_MASTER		
+#define SPI_DORD		SPI_DORD_LSB
+
+#endif /* SPI_CONFIGURATION_H_ */
