65d9a79fbbeb2e4e874c0c1237782e6326b0c389
mazentamer <mazentamer74@yahoo.com>
2021-09-30 12:43:27 +0200

    Create SPI_Address.h

---
+++ FinalProject_Master/FinalProject_Master/MCAL/SPI_Address.h
@@ -0,0 +1,18 @@
+/*
+ * SPI_Address.h
+ *
+ * Created: 04/09/2021 21:46:17
+ *  Author: user
+ */ 
+
+
+#ifndef SPI_ADDRESS_H_
+#define SPI_ADDRESS_H_
+
+#include "CPU_Configurations.h"
+/*
+#define SPDR	(*(volatile uint8_t*)0x2F)
+#define SPSR	(*(volatile uint8_t*)0x2E)
+#define SPCR	(*(volatile uint8_t*)0x2D)
+*/
+#endif /* SPI_ADDRESS_H_ */
