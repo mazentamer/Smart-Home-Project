4031f120728173f2dafb27bf9afbec9798cd6fa8
mazentamer <mazentamer74@yahoo.com>
2021-09-30 12:44:32 +0200

    Create makedep.mk

---
+++ FinalProject_Slave/FinalProject_Slave/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\main.c
+
+HAL\LCD.c
+
+HAL\OUTPUT_Module.c
+
+MCAL\DIO.c
+
+MCAL\SPI.c
+
