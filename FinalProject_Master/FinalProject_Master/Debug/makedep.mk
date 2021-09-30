9239a8bd3b8a1f689cac10aee413808bdef19937
mazentamer <mazentamer74@yahoo.com>
2021-09-30 12:34:11 +0200

    Create makedep.mk

---
+++ FinalProject_Master/FinalProject_Master/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\main.c
+
+HAL\LCD.c
+
+HAL\OUTPUT_Module.c
+
+MCAL\DIO.c
+
+MCAL\SPI.c
+
+MCAL\USART.c
+
